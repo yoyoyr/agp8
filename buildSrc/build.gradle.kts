c34f6e4abe4ce23f0a04b8b71a1168eae86e5403
杨荣 <yr5@meitu.com>
2025-01-10 10:45:50 +0800

    asm仓库引入

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("groovy") // 引入 Groovy 插件
     kotlin("jvm") version "1.9.24"
 }
 
@@ -8,5 +7,5 @@ repositories {
 }
 
 dependencies {
-    implementation("org.codehaus.groovy:groovy-all:3.0.9") // 确保依赖也在主项目中
+    implementation("org.ow2.asm:asm:9.6")
 }
